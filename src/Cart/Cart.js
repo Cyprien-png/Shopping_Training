028d3bd869e9b6ecf6ae8d3cda547a94dcf3fb5f
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-02-01 10:27:00 +0100

    add: the list of items in the Cart

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,17 +1,26 @@
 "use strict";
 
+const EmptyCartException = require("./EmptyCartException.js");
+
 module.exports = class Cart {
 
     //region private attributes
-
+    _items;
     //endregion private attributes
 
     //region public methods
-    constructor(articleId, name, quantity, price) {
-
+    constructor(items) {
+        this.items = items;
     }
 
     //endregion public methods
+    get items() {
+        return this._items;
+    }
+
+    set items(value) {
+        this._items = value;
+    }
 
     //region private methods
     //endregion private methods
